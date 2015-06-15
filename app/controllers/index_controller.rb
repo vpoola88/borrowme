e5561bbc2e98189a78eb67a0999e1bfef9e2bbfa
Vivek Poola <vpoola88@gmail.com>
2015-06-15 09:39:58 -0700

    split controllers and updated css

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -11,58 +11,6 @@ get '/' do
 end
 
 
-get '/categories' do
-
-  redirect '/login' unless session[:user_id]
-
-  @categories = Category.all
-  erb :categories
-
-end
-
-# CREATE
-get '/categories/:category_id/items' do
-
-  @category = Category.where(id: params[:category_id])
-  @items = Item.where(category_id: params[:category_id])
-
-  erb :items
-end
-
-post '/categories/:category_id/items' do
-
-  @item = Item.create(name: params[:name], description: params[:description], price: params[:price], category_id: params[:category_id], user_id: session[:user_id])
-
-  redirect "/categories/#{params[:category_id]}/items"
-end
-
-# UPDATE
-
-get '/categories/:category_id/items/:item_id' do
-
-  @item = Item.find_by(id: params[:item_id])
-
-  erb :update_item
-
-end
-
-put '/categories/:category_id/items/:item_id' do
-  item = Item.find_by(id: params[:item_id])
-  item.update_attributes(name: params[:name], description: params[:description], price: params[:price])
-
-  redirect "/categories/#{params[:category_id]}/items"
-end
-
-
-# DELETE
-delete '/categories/:category_id/items/:item_id' do
-  item = Item.find(params[:post_id])
-  item.destroy
-
-  redirect to "/categories/#{params[:category_id]}/items"
-end
-
-
 post '/search' do
 
   @search = Item.where(name: params[:search])
